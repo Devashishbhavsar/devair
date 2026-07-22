ff5644c61a95937ab214c8bc5c0a80c6e9797114
Devashish Bhavsar <dev.softvan@gmail.com>
2026-07-23 04:46:15 +0530

    chore: bind product repo for Paperclip workspace

---
+++ public/global-header.tsx
@@ -0,0 +1,10 @@
+// Global Header component
+class GlobalHeader extends React.Component {
+  render() {
+    return (
+      <header className="global-header">
+        {/* Your header content here */}
+      </header>
+    );
+  }
+}
