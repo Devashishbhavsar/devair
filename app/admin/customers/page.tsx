71691d947c8da897d1ae5a4ea3fe3c22452bf026
Devashish Bhavsar <dev.softvan@gmail.com>
2026-07-22 13:02:51 +0530

    chore: bind product repo for Paperclip workspace

---
+++ app/admin/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomersPage = () => {
+  return (
+    <div>
+      <h1>Customer List</h1>
+      {/* Customer list and refund request flow implementation goes here */}
+    </div>
+  );
+};
+
+export default CustomersPage;
